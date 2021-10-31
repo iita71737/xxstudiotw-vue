8af2bf96b5280de1fa61f2e04814b720d3001289
lin chihhao <iita71737@gmail.com>
2021-10-31 20:43:25 +0800

    get from localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,7 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    shoppingCart: []
+    shoppingCart: JSON.parse(localStorage.getItem("XXcart") || "[]")
   },
   mutations: {
     addToCart(state, product) {
